ea8fdd4eb6646d26fbc6a2ec99f9c71c07e8405b
Mathieu Noyelle <math-and-rowing@hotmail.fr>
2021-04-14 01:07:23 +0200

    :hammer: Ajout script postinstall v0

---
+++ autoinstall.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+
+# Nom : postinstall.sh 
+# Auteur : Mathieu NOYELLE
+# contact : mathieu@noyelle.pro
+
+create_ssh_key(){
+	##########################################################
+	#							 #
+	#	Création des clés ssh pour un utilisateur	 #
+	#							 #
+	##########################################################
+
+	# Param1 = nom d'utilisateur
+
+	user=$1 
+	
+	if [ $user = "root" ]; then 
+		ssh_path=/root/.ssh
+	else
+		ssh_path=/home/$user/.ssh
+	fi 
+	
+	mkdir -v $ssh_path
+	chmod -v 700 $ssh_path
+	ssh-keygen -t ed25519 -f $ssh_path/id_ed25519 -q -N ""
+	chown -R $user:$user $ssh_path
+
+}
+
+packager(){
+	##########################################################
+	#							 #
+	#	Gestion des paquets (Clean & Install)		 #
+	#							 #
+	##########################################################
+
+	# Mise à jour des paquets 
+	apt update -y 
+	apt upgrade -y 
+	
+	# Suppression des paquets non nécessaires 
+	apt remove task-laptop telnet -y
+	
+	# installation des paquets essentiels 
+	apt install -y mlocate rsync htop net-tools vim tmux screen zip pigz pixz \
+		       dstat iotop git psmisc tree lynx at postfix lshfw inxi figlet \ 
+	       		gdisk mc cifs-utils ntfs-3g sudo curl sshfs apt-file openssl \ 
+	       		gnupg2 dnsutils fish gpm grc ncdu p7zip-full parted
+
+	# Netoyage après installation 
+	apt autoremove -y
+	apt clean -y 
+}
+
+
+create_user(){
+	##########################################################
+	#							 #
+	#		Création d'un utilisateur 		 #
+	#							 #
+	##########################################################
+	
+	# Param1 = nom d'utilisateur 
+	# Param2 = GID 
+	# Param3 = UID 
+	# Param4 = Mot de passe 
+	# Param5 = sudo / nosudo
+
+	username=$1
+	gid=$2
+	uid=$3
+	password=$4
+	admin=$5
+
+	groupadd -g $gid $username
+	useradd -u $uid -g $gid -m $username
+	echo -e "$password\n$password" | passwd $username 
+
+	if [ $admin = sudo ]; then 
+		usermod -aG sudo $username
+	fi 
+}
+
+
+config_ssh(){
+	##########################################################
+	#							 #
+	#		Création d'un utilisateur 		 #
+	#							 #
+	##########################################################
+
+	echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+	echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
+}
+
+set_default_applications(){
+	##########################################################
+	#							 #
+	# 	Définitions des applications par défaut	 	 #
+	#							 #
+	##########################################################
+
+	ln -sfn /usr/bin/vim.basic /etc/alternatives/editor
+
+}
+
+set_default_ip(){
+	##########################################################
+	#							 #
+	# 	Définitions des applications par défaut	 	 #
+	#							 #
+	##########################################################
+
+	# Param1 = adresse IP 
+	# Param2 = netmask 
+	# Param3 = gateway 
+	# Param4 = dns serveurs 
+
+	address=$1
+	mask=$2
+	gateway=$3
+	dns=$4
+
+	interface=`ip -o link show | awk -F': ' '{print $2}' | grep -v lo`
+	sed -i 's/dhcp/static/' /etc/network/interfaces
+
+	cat >> /etc/network/interfaces.d/$interface << EOF
+# Ajout de l'interface $interface
+allow-hotplug $interface
+iface $interface inet static
+	address $address
+	netmask $mask 
+	gateway $gateway
+	dns-nameservers $dns 
+EOF
+
+	ifdown $interface
+	ifup $interface
+
+	systemctl -w net.ipv6.conf.$interface.disable_ipv6=1
+	
+}
+
+
+set_banner(){
+	##########################################################
+	#							 #
+	# 	Mise en place d'une banière connexion		 #
+	#							 #
+	##########################################################
+	
+	cat > /etc/issue.net << EOF
+Toute utilisation non autorisée sera sévèrement punie 
+EOF 
+	cp /etc/issue.net /etc/issue
+	figlet WIKI-COFFRE > /etc/motd
+}
+
+set_ntp_on(){
+	##########################################################
+	#							 #
+	#	Activer le service NTP 				 #
+	#							 #
+	##########################################################
+
+	timedatectl set-ntp on 
+	systemctl status systemd-timesyncd
+	systemctl restart systemd-timesyncd
+}
+
+postinstall_ESGI_work(){
+	packager 
+	create_ssh_key root
+	create_user esgi 10000 10000 P@ssword sudo
+	create_ssh_key esgi 
+	create_ssh_key $USER
+	set_default_applications
+	config_ssh 
+	set_default_ip 192.168.1.190 255.255.255.0 192.168.1.254 "1.1.1.1 9.9.9.9"
+	set_banner
+	set_ntp_on
+}
+
+postinstall_ESGI_work
